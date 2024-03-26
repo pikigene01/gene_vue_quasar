7edb8414e1b0d1d074cb41b2c97788a73a8e3740
Gene Piki <85885450+pikigene01@users.noreply.github.com>
2024-03-26 22:52:39 +0200

    update gene img ui

--- src/components/YouTubeHome.vue
+++ src/components/YouTubeHome.vue
@@ -10,57 +10,55 @@
       <q-page-container>
         <div class="q-pa-md">
           <div class="row">
-            <div v-for="video in videos" :key="video?.id">
-              <div class="col-6">
-                <q-item>
-                  <q-item-section>
-                    <q-img
-                      :src="video?.img"
-                      :style="{
-                        height: '200px',
-                        width: '250px',
-                        'border-radius': '10px',
-                      }"
-                    />
-                    <div
-                      class="flex"
-                      :style="{
-                        display: 'flex',
-                        'flex-direction': 'row',
-                        'margin-top': '10px',
-                      }"
-                    >
-                      <div class="avater">
-                        <div>
-                          <q-item>
-                            <q-item-section top avatar>
-                              <q-avatar>
-                                <img src="/public/assets/icons/avatar.png" />
-                              </q-avatar>
-                            </q-item-section>
+            <div v-for="video in videos" :key="video?.id" class="col-3">
+              <q-item>
+                <q-item-section>
+                  <q-img
+                    :src="video?.img"
+                    :style="{
+                      height: '200px',
+                      width: '100%',
+                      'border-radius': '10px',
+                    }"
+                  />
+                  <div
+                    class="flex"
+                    :style="{
+                      display: 'flex',
+                      'flex-direction': 'row',
+                      'margin-top': '10px',
+                    }"
+                  >
+                    <div class="avater">
+                      <div>
+                        <q-item>
+                          <q-item-section top avatar>
+                            <q-avatar>
+                              <img src="/public/assets/icons/avatar.png" />
+                            </q-avatar>
+                          </q-item-section>
 
-                            <q-item-section>
-                              <q-item-label>{{ video?.title }}</q-item-label>
-                              <q-item-label caption>{{
-                                video?.descrition
-                              }}</q-item-label>
-                              <q-item-label caption>{{
-                                video?.created_at
-                              }}</q-item-label>
-                            </q-item-section>
+                          <q-item-section>
+                            <q-item-label>{{ video?.title }}</q-item-label>
+                            <q-item-label caption>{{
+                              video?.descrition
+                            }}</q-item-label>
+                            <q-item-label caption>{{
+                              video?.created_at
+                            }}</q-item-label>
+                          </q-item-section>
 
-                            <q-item-section side top>
-                              <q-badge label="10k" />
-                            </q-item-section>
-                          </q-item>
+                          <q-item-section side top>
+                            <q-badge label="10k" />
+                          </q-item-section>
+                        </q-item>
 
-                          <q-separator spaced inset="item" />
-                        </div>
+                        <q-separator spaced inset="item" />
                       </div>
                     </div>
-                  </q-item-section>
-                </q-item>
-              </div>
+                  </div>
+                </q-item-section>
+              </q-item>
             </div>
           </div>
         </div>
